b1a6c2bd78bab88605120d03457e90937919238b
wellington <wellington.h.s.oliveira@hotmail.com>
2025-06-27 17:27:40 -0300

    implementando o metodo somar no endPoint

---
+++ src/main/java/com/example/wellington/udemy/controllers/MathController.java
@@ -0,0 +1,42 @@
+package com.example.wellington.udemy.controllers;
+
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/math")
+public class MathController {
+
+    //http://localhost:8080/math/sum/
+    @RequestMapping("/sum/{numberOne}/{numberTwo}")
+    public Double sum(
+            @PathVariable("numberOne") String numberOne,
+            @PathVariable("numberTwo") String numberTwo
+    ) throws Exception {
+        if (!isNumeric(numberOne)|| !isNumeric(numberTwo)) throw new IllegalArgumentException();
+
+        return convertToDouble (numberOne) + convertToDouble (numberTwo);
+    }
+
+    private Double convertToDouble(String strnumber) {
+        if (strnumber == null || strnumber.isEmpty()) throw new IllegalArgumentException();
+        String number = strnumber.replace(",", ".");
+        return Double.parseDouble(number);
+
+    }
+
+    private boolean isNumeric(String strnumber) {
+        if (strnumber == null || strnumber.isEmpty()) return false;
+        String number = strnumber.replace(",", ".");
+        return number.matches("[-+]?[0-9]*\\.?[0-9]+");
+
+    }
+
+
+    //http://localhost:8080/math/subtraction/
+    //http://localhost:8080/math/division/
+    //http://localhost:8080/math/sum/
+    //http://localhost:8080/math/sum/
+
+}
