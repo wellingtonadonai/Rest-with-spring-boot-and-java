5579536f645b9bacae23ecfafe326b398af7449a
wellington <wellington.h.s.oliveira@hotmail.com>
2025-07-11 20:05:10 -0300

    refatorei os controller e serviços

--- src/main/java/com/example/wellington/udemy/controllers/PersonController.java
+++ src/main/java/com/example/wellington/udemy/controllers/PersonController.java
@@ -1,7 +1,7 @@
 package com.example.wellington.udemy.controllers;
 
+import com.example.wellington.udemy.data.dto.PersonDTO;
 import com.example.wellington.udemy.service.PersonServices;
-import com.example.wellington.udemy.model.Person;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
@@ -16,26 +16,26 @@ public class PersonController {
     private PersonServices service;
 
     @GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
-    public List<Person> findAll(){
+    public List<PersonDTO> findAll(){
         return service.findAll();
 
     }
 
     @GetMapping(value = "/{id}",
     produces = MediaType.APPLICATION_JSON_VALUE)
-    public Person findById(@PathVariable("id")Long id){
+    public PersonDTO findById(@PathVariable("id")Long id){
         return service.findById(id);
 
     }
     @PostMapping(consumes = MediaType.APPLICATION_JSON_VALUE,
     produces = MediaType.APPLICATION_JSON_VALUE)
-    public Person create (@RequestBody Person person){
-        return service.create(person);
+    public PersonDTO create (@RequestBody PersonDTO Person){
+        return service.create(Person);
 
     }
     @PutMapping(consumes = MediaType.APPLICATION_JSON_VALUE,
     produces = MediaType.APPLICATION_JSON_VALUE)
-    public Person update (@RequestBody Person person){
+    public PersonDTO update (@RequestBody PersonDTO person){
         return service.update(person);
 
     }
--- src/main/java/com/example/wellington/udemy/service/PersonServices.java
+++ src/main/java/com/example/wellington/udemy/service/PersonServices.java
@@ -1,6 +1,9 @@
 package com.example.wellington.udemy.service;
 
+import com.example.wellington.udemy.data.dto.PersonDTO;
 import com.example.wellington.udemy.exceptions.ResourceNotFoundException;
+import static com.example.wellington.udemy.mapper.ObjectMapper.parseListObject;
+import static com.example.wellington.udemy.mapper.ObjectMapper.parseObject;
 import com.example.wellington.udemy.model.Person;
 import com.example.wellington.udemy.repositories.PersonRepositories;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -20,41 +23,44 @@ public class PersonServices {
 
 
 
-    public List<Person> findAll(){
+    public List<PersonDTO> findAll(){
         logger.info("Finding all People");
-        return repositories.findAll();
+        return parseListObject(repositories.findAll(), PersonDTO.class);
 
 
     }
 
 
 
-    public Person findById(Long id){
+    public PersonDTO findById(Long id){
         logger.info("Finding one Person");
 
-         return repositories.findById(id)
+         var entity = repositories.findById(id)
                  .orElseThrow(()-> new ResourceNotFoundException("No records found for this id"));
+         return parseObject(entity, PersonDTO.class);
 
 
 
     }
 
-    public Person create(Person person){
+    public PersonDTO create(PersonDTO person){
         logger.info("create one Person");
-        return repositories.save(person);
+        var entity = parseObject(person, Person.class);
+
+        return parseObject (repositories.save(entity),PersonDTO.class);
 
     }
 
-    public Person update(Person person) {
+    public PersonDTO update(PersonDTO person) {
         logger.info("update one Person");
         Person entity = repositories.findById(person.getId())
-                .orElseThrow(()-> new ResourceNotFoundException("No records found for this id"));
+                .orElseThrow(() -> new ResourceNotFoundException("No records found for this id"));
         entity.setFirstname(person.getFirstname());
-        entity.setLasttname (person.getLasttname());
+        entity.setLasttname(person.getLasttname());
         entity.setAdress(person.getAdress());
         entity.setGender(person.getGender());
 
-        return repositories.save(person);
+        return parseObject(repositories.save(entity),PersonDTO.class);
     }
     public void delete (Long id) {
         logger.info("delete one Person");
