7d7ab3c7fc6db04f1052769d0a60b1ac4bd59853
wellington <wellington.h.s.oliveira@hotmail.com>
2025-07-11 19:26:27 -0300

    add classes de tests para verificar se está acontecendo a conversão em mapper

---
+++ src/test/java/com/example/wellington/udemy/unitetest/mapper/mocks/MockPerson.java
@@ -0,0 +1,57 @@
+package com.example.wellington.udemy.unitetest.mapper.mocks;
+
+import com.example.wellington.udemy.data.dto.PersonDTO;
+import com.example.wellington.udemy.model.Person;
+
+import java.util.ArrayList;
+import java.util.List;
+
+
+public class MockPerson {
+
+
+    public Person mockEntity() {
+        return mockEntity(0);
+    }
+    
+    public PersonDTO mockDTO() {
+        return mockDTO(0);
+    }
+    
+    public List<Person> mockEntityList() {
+        List<Person> persons = new ArrayList<Person>();
+        for (int i = 0; i < 14; i++) {
+            persons.add(mockEntity(i));
+        }
+        return persons;
+    }
+
+    public List<PersonDTO> mockDTOList() {
+        List<PersonDTO> persons = new ArrayList<>();
+        for (int i = 0; i < 14; i++) {
+            persons.add(mockDTO(i));
+        }
+        return persons;
+    }
+    
+    public Person mockEntity(Integer number) {
+        Person person = new Person();
+        person.setAdress("Address Test" + number);
+        person.setFirstname("First Name Test" + number);
+        person.setGender(((number % 2)==0) ? "Male" : "Female");
+        person.setId(number.longValue());
+        person.setLasttname("Last Name Test" + number);
+        return person;
+    }
+
+    public PersonDTO mockDTO(Integer number) {
+        PersonDTO person = new PersonDTO();
+        person.setAdress("Address Test" + number);
+        person.setFirstname("First Name Test" + number);
+        person.setGender(((number % 2)==0) ? "Male" : "Female");
+        person.setId(number.longValue());
+        person.setLasttname("Last Name Test" + number);
+        return person;
+    }
+
+}
