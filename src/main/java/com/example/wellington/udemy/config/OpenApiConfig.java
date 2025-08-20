23c29d918710cd8d5a859954b004202332dcd2fb
wellington <wellington.h.s.oliveira@hotmail.com>
2025-08-19 23:11:34 -0300

    Implementando documentação swagger openAPI

---
+++ src/main/java/com/example/wellington/udemy/config/OpenApiConfig.java
@@ -0,0 +1,26 @@
+package com.example.wellington.udemy.config;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class OpenApiConfig {
+
+    @Bean
+    OpenAPI customOpenApi(){
+        return new OpenAPI()
+        .info(new Info().title("REST API's RESTful from 0 witch java, Spring Boot, kubernets and Docker")
+                .version("V1")
+                .description("REST API's RESTful from 0 witch java, Spring Boot, kubernets and Docker")
+                .termsOfService("https://github.com/wellingtonadonai")
+                .license(new License()
+                        .name("Apache 2.0")
+                        .url("https://github.com/wellingtonadonai")
+                )
+        );
+    }
+
+}
--- src/main/java/com/example/wellington/udemy/controllers/PersonController.java
+++ src/main/java/com/example/wellington/udemy/controllers/PersonController.java
@@ -1,7 +1,9 @@
 package com.example.wellington.udemy.controllers;
 
+import com.example.wellington.udemy.controllers.docs.PersonControllerDocs;
 import com.example.wellington.udemy.data.dto.PersonDTO;
 import com.example.wellington.udemy.service.PersonServices;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
@@ -10,31 +12,36 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/api/person/v1")
-public class PersonController {
+@Tag(name ="People", description = "Endipoints for Managing People")
+public class PersonController implements PersonControllerDocs {
     @Autowired
     private PersonServices service;
+
 
     @GetMapping(produces = {
     MediaType.APPLICATION_JSON_VALUE,
     MediaType.APPLICATION_XML_VALUE,
     MediaType.APPLICATION_YAML_VALUE})
 
+    @Override
     public List<PersonDTO> findAll(){
         return service.findAll();
 
     }
 
+    @Override
     @GetMapping(value = "/{id}",
     produces = {
     MediaType.APPLICATION_JSON_VALUE,
     MediaType.APPLICATION_XML_VALUE,
     MediaType.APPLICATION_YAML_VALUE})
 
-    public PersonDTO findById(@PathVariable("id")Long id){
+    public PersonDTO findById(@PathVariable("id") Long id){
         return service.findById(id);
 
 
     }
+    @Override
     @PostMapping(
     consumes = {
     MediaType.APPLICATION_JSON_VALUE,
@@ -46,11 +53,12 @@ public class PersonController {
     MediaType.APPLICATION_XML_VALUE,
     MediaType.APPLICATION_YAML_VALUE})
 
-    public PersonDTO create (@RequestBody PersonDTO Person){
+    public PersonDTO create(@RequestBody PersonDTO Person){
         return service.create(Person);
 
     }
 
+    @Override
     @PutMapping(
     consumes = {
     MediaType.APPLICATION_JSON_VALUE,
@@ -62,13 +70,14 @@ public class PersonController {
     MediaType.APPLICATION_XML_VALUE,
     MediaType.APPLICATION_YAML_VALUE})
 
-    public PersonDTO update (@RequestBody PersonDTO person){
+    public PersonDTO update(@RequestBody PersonDTO person){
         return service.update(person);
 
     }
 
+    @Override
     @DeleteMapping(value = "/{id}")
-    public ResponseEntity<?> delete (@PathVariable("id") Long id){
+    public ResponseEntity<?> delete(@PathVariable("id") Long id){
         service.delete(id);
         return ResponseEntity.noContent().build();
     }
